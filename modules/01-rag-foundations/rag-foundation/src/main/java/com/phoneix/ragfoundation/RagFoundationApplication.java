ad150833e390fd048149c19ac60b592922b9f5f7
Raúl Ferrer <raul.ferrer.dev@gmail.com>
2026-03-25 07:46:04 +0100

    Added auditor & rag-foundations projects

---
+++ modules/01-rag-foundations/rag-foundation/src/main/java/com/phoneix/ragfoundation/RagFoundationApplication.java
@@ -0,0 +1,13 @@
+package com.phoneix.ragfoundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagFoundationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagFoundationApplication.class, args);
+    }
+
+}
